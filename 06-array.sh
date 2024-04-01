d3c7949bc21000927954a4603e74ee4f7db43bf1
Anill <info@joindevops.com>
2024-04-01 23:34:48 +0530

    2nd time added

--- 06-array.sh
+++ 06-array.sh
@@ -3,3 +3,4 @@ PERSONS=("Ramesh" "Suresh" "Vinod")
 echo "First Person is: ${PERSONS[0]}"
 echo "Second Person is: ${PERSONS[1]}"
 echo "Third Person is: ${PERSONS[2]}"
+echo "All Persons is: ${PERSONS[@]}"
