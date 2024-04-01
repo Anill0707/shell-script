b5927737a9511c50d649d3ac5d9a0b5b9787708b
Anill <info@joindevops.com>
2024-04-01 22:29:29 +0530

    2nd time added

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "I am Learning Devops and Linux"
-Person1=Ramesh
-Person2=Suresh
+Person1=Sachin
+Person2=Rohith
 echo "$Person1: Hello $Person1, How are you"
 echo "$Person2: Hello $Person1, How are you"
