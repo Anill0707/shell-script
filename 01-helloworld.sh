4700d8169da15f10730159b5abc31a0c6c40c1b1
Anill <info@joindevops.com>
2024-04-01 22:30:29 +0530

    2nd time added

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -2,5 +2,5 @@
 echo "I am Learning Devops and Linux"
 Person1=Sachin
 Person2=Rohith
-echo "$Person1: Hello $Person1, How are you"
+echo "$Person1: Hello $Person2, How are you"
 echo "$Person2: Hello $Person1, How are you"
