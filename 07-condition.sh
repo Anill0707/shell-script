2e48e2293d4de05f513fad7e004f91570c9fbc87
Anill <info@joindevops.com>
2024-04-02 12:21:02 +0530

    2nd time added

--- 07-condition.sh
+++ 07-condition.sh
@@ -3,9 +3,11 @@ num=$1
 if [ $num -gt 10 ]
 then
  echo "the given $num is greater than 10"
-elif [ $num == 10]
-then
-echo "the given number$num is equal to 10"
 else
-echo " the given $num is less than 10"
+ if [ $num == 10]
+ then
+ echo "the given number$num is equal to 10"
+ fi
+else
+echo "the given $num is less than 10"
 fi 
